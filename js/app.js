a56395e164dd15b32d63427e6b628ceb73930e09
Yhosi Esteban Dulcey Garcia <ydulceyg@sena.edu.co>
2026-05-06 12:01:18 -0500

    feat: Agregar funcionalidad para crear tareas

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("TaskFlow iniciado");
+
+function addTask() {
+    const input = document.getElementById("taskInput");
+    const taskText = input.value;
+
+    if (taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
